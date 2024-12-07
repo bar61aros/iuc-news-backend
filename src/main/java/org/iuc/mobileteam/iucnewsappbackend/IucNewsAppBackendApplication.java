fc0634708a98265b319977d4415891b66dffeaf0
barbaros yıldırım <barbarosayildirim@gmail.com>
2024-12-08 00:34:02 +0300

    chore: init project

---
+++ src/main/java/org/iuc/mobileteam/iucnewsappbackend/IucNewsAppBackendApplication.java
@@ -0,0 +1,13 @@
+package org.iuc.mobileteam.iucnewsappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IucNewsAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IucNewsAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/iuc/mobileteam/iucnewsappbackend/IucNewsAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iuc.mobileteam.iucnewsappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IucNewsAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
