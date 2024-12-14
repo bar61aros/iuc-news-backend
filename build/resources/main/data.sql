4f9c1519f2490aa5ea8362ad06911ebd6bd02462
barbaros yıldırım <barbarosayildirim@gmail.com>
2024-12-14 21:38:51 +0300

    chore: rm build folder

--- build/resources/main/data.sql
+++
@@ -1,5 +0,0 @@
--- Insert initial data into the news table
-INSERT INTO news (title, content) VALUES
-                                      ('Breaking News: Kotlin Spring Boot Rocks!', 'Kotlin with Spring Boot is a modern and concise way to build robust REST APIs.'),
-                                      ('Tech Update: MySQL Integration', 'Spring Boot seamlessly integrates with MySQL, making database interactions smooth and efficient.'),
-                                      ('Programming Tips', 'Always write clean, readable, and maintainable code for a better development experience.');
