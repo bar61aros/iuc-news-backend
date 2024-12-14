7c3045cd8efe024e39e1ab0d699de422b70ff496
barbaros yıldırım <barbarosayildirim@gmail.com>
2024-12-14 15:13:04 +0300

    chore: add new proj init files

---
+++ src/main/kotlin/org/iuc/mobileteam/iucnewsbackend/IucNewsBackendApplication.kt
@@ -0,0 +1,11 @@
+package org.iuc.mobileteam.iucnewsbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IucNewsBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<IucNewsBackendApplication>(*args)
+}
