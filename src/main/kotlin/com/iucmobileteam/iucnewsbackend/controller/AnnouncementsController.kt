c3415fec2480619dfe3c9ba2b3139d9206146382
barbaros yıldırım <barbarosayildirim@gmail.com>
2024-12-15 14:18:19 +0300

    feat: add logger and exception handler

--- src/main/kotlin/com/iucmobileteam/iucnewsbackend/controller/AnnouncementsController.kt
+++ src/main/kotlin/com/iucmobileteam/iucnewsbackend/controller/AnnouncementsController.kt
@@ -12,10 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 
 @RestController
 @RequestMapping("announcements")
 class AnnouncementsController (private val announcementsService: AnnouncementsService) {
+    private val logger: Logger = LoggerFactory.getLogger(AnnouncementsController::class.java)
+
     @GetMapping
     fun getAnnouncements(): MutableList<AnnouncementsEntity> = announcementsService.getAnnouncements()
 
@@ -23,11 +30,20 @@ class AnnouncementsController (private val announcementsService: AnnouncementsSe
     fun getAnnouncementById(@PathVariable id: Long): Optional<AnnouncementsEntity> = announcementsService.getAnnouncementById(id)
 
     @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
     fun createAnnouncement(@RequestBody announcement: AnnouncementsDto): AnnouncementsEntity = announcementsService.createAnnouncement(announcement.toAnnouncementsEntity())
 
     @PostMapping("bulk")
+    @ResponseStatus(HttpStatus.CREATED)
     fun updateAnnouncements(@RequestBody announcements: List<AnnouncementsDto>): MutableList<AnnouncementsEntity> = announcementsService.updateAnnouncements(announcements.map { it.toAnnouncementsEntity() })
 
     @DeleteMapping()
     fun deleteAllAnnouncements() = announcementsService.deleteAllAnnouncements()
+
+    @ExceptionHandler(Exception::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    final fun handleAllExceptions(e: Exception): Exception {
+        logger.error("An error occurred: ${e.message}")
+        return e
+    }
 }
